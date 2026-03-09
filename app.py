b728d9e61b105a88cd9af447a9b93a7776a2c73f
codeinveins123 <leplaygame123@gmail.com>
2026-03-09 13:52:48 +0500

    fix class

--- app.py
+++ app.py
@@ -91,12 +91,12 @@ if st.button("Запустить"):
     shutil.move(AGENTS_PATH+".bak", AGENTS_PATH)
     shutil.move(TASKS_PATH+".bak", TASKS_PATH)
 
-    PLAN_PATH = os.path.join(BASE_DIR, "iws/plan.txt")  # путь к твоему файлу plan.txt
+    PLAN_PATH = os.path.join(BASE_DIR, "iws2/plan.txt")
 
     if os.path.exists(PLAN_PATH):
         with open(PLAN_PATH, "r", encoding="utf-8") as f:
             plan_content = f.read()
-        st.subheader("Содержимое iws/plan.txt:")
-        st.text(plan_content)  # можно st.markdown(plan_content) если есть форматирование
+        st.subheader("Ответ эдвайзера:")
+        st.text(plan_content)
     else:
         st.warning(f"Файл {PLAN_PATH} не найден.")
